f81ae2129b5de2de16ccb8898710ff61c74d7c2e
JairZijp <jairzijp123@gmail.com>
2018-10-24 13:34:45 +0200

    reaallly small setup

--- src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
+++ src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
@@ -5,14 +5,28 @@ import nl.hva.ict.ds.Player;
 import java.util.List;
 
 public class LinearProbingMultiValueSymbolTable implements MultiValueSymbolTable<String, Player> {
+    
+    private int maxArraySize;
+    private int arraySize;
+    
     public LinearProbingMultiValueSymbolTable(int arraySize) {
+        this.maxArraySize = arraySize;
     }
 
     @Override
     public void put(String key, Player value) {
         
-        
-
+        int hash = hash(key);
+        System.out.println("Hash: " + hash);
+           
+    }
+    
+    private int hash(String key) {
+        return key.hashCode() % maxArraySize; 
+    }
+    
+    private int size() {
+        return arraySize;
     }
 
     @Override
